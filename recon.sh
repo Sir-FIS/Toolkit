0b8fc13bfad9a990e662f6649f9388350fa0fd82
Sir_FIS <marwan.elgendi@yahoo.com>
2019-06-12 15:14:40 +0100

    Add files via upload
    recon bash script using nmap and nbtscan

---
+++ recon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+IP="$1"
+nmap -F  -sS  $IP -vv  -n  -oN fast_"$IP"  &
+nmap -sS   $IP -vv -n --top-ports 1000 -oN slow-"$IP"  &
+nmap -sU $IP -vv  -n --max-retries=3 -oN  udp-"$IP"  &
+nmap   $IP -vv  -n -sV --version-light -oN banners-"$IP" &
+nmap $IP -R -vv -sn -oN dns-"$IP" &
+nmap $IP  --script smb-enum-shares -vv  -oN smb_enum-"$IP" &
+nmap $IP -O -T4 -n -vv -oN OS-"$IP" &
+nbtscan -vh $IP > nbt-scan
